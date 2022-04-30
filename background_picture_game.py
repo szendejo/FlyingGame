8765325c46ce807ad68415fe82580b8db1e13903
Stephanie <szacademia@gmail.com>
2022-04-30 13:29:24 -0500

    Fix background

--- background_picture_game.py
+++ background_picture_game.py
@@ -1,104 +1,46 @@
 import pygame
-
 import sys
-
 import os
-
+import math
 from pygame.locals import *
 
-class ScrollingBackground:
-
-    def __init__(self, screenwidth, imagefile):
-
-        self.img = pygame.image.load(imagefile)
-
-        self.coord = [0, 0]
-
-        self.coord2 = [-screenwidth, 0]
-
-        self.x_original = self.coord[0]
-
-        self.x2_original = self.coord2[0]
-
-    def Show(self, surface):
-
-        surface.blit(self.img, self.coord)
-
-        surface.blit(self.img, self.coord2)
-
-    def UpdateCoords(self, speed_x, time):
-
-        distance_x = speed_x * time
-
-        self.coord[0] += distance_x
-
-        self.coord2[0] += distance_x
-
-        if self.coord2[0] >= 0:
-
-            self.coord[0] = self.x_original
-
-            self.coord2[0] = self.x2_original
-
-
-
-
-pygame.init()  # initialize pygame
-
+pygame.init()
 clock = pygame.time.Clock()
-
 screenwidth, screenheight = (500, 280)
-
 screen = pygame.display.set_mode((screenwidth, screenheight))
-
-
-
-# Set the framerate
-
-framerate = 60
-
-
-
-# Set the background scrolling speed
-
-bg_speed = 100
-
-
-
-# Load the background image here. Make sure the file exists!
-
-Sky = ScrollingBackground(screenheight, "BlueGameBackground.png")
-
+bg = pygame.image.load("BlueGameBackgroundRepeatingV3.png")
+bgX = 0
+bgX2 = bg.get_width()
 pygame.mouse.set_visible(0)
-
 pygame.display.set_caption('Let\'s Fly!')
 
+def redrawWindow():
+    screen.blit(bg, (bgX, 0))  # draws our first bg image
+    screen.blit(bg, (bgX2, 0))  # draws the seconf bg image
+    pygame.display.update()  # updates the screen
 
-
-# fix indentation
-
-while True:
-
-    time = clock.tick(framerate)/1000.0
-
+run = True
+speed = 30
+while run:
+    redrawWindow()
+    bgX -= 1.4
+    bgX2 -= 1.4
+    if bgX < bg.get_width() * -1:  # If our bg is at the -width then reset its position
+        bgX = bg.get_width()
+    
+    if bgX2 < bg.get_width() * -1:
+        bgX2 = bg.get_width()
+    
     x, y = pygame.mouse.get_pos()
 
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+            quit()
 
-            sys.exit()
+    clock.tick(speed)
 
-
-
-    # Set new Background Coordinates and update the screen
-
-    Sky.UpdateCoords(bg_speed, time)
-
-    Sky.Show(screen)
-
-    pygame.display.update()
 
 
 
